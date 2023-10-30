5326a6f0313c97e83b913cd815f196acc9da7234
FenrisGandr <144750146+FenrisGandr@users.noreply.github.com>
2023-10-30 01:28:39 -0700

    refactored second opinion page to use our api

--- src/components/second opinion components/SecondOpinionCenter.jsx
+++ src/components/second opinion components/SecondOpinionCenter.jsx
@@ -1,153 +1,57 @@
-import React from "react"
-import doctor1 from '../../assets/doctor1.png';
-import doctor2 from '../../assets/doctor2.png';
-import doctor3 from '../../assets/doctor3.png';
-import doctor4 from '../../assets/doctor4.png';
-import doctor5 from '../../assets/doctor5.png';
-import doctor6 from '../../assets/doctor6.png';
+import React, { useEffect, useState} from "react"
+import { useAuth } from "../../contexts/AuthContext";
 import SecondOpinionSection from "./SecondOpinionSection";
 
 function SecondOpinionCenter(){
-    const doctors = [
-        {
-            picture: doctor1,
-            doctorAndTitle: "Dr. Allison Mitchell, MD",
-            specialization: "Diagnostic Radiology",
-            additionalText: ["Greetings,","I am Dr. Allison Mitchell, a seasoned radiologist specializing in diagnostic radiology. With a career spanning over two decades, my journey in healthcare has been driven by a passion for precision, a commitment to compassionate care, and an unyielding dedication to advancing the field of medical imaging.",
-                            "Mastery in Diagnostic Radiology:", 
-                            "As a diagnostic radiologist, I possess expertise in a comprehensive range of imaging techniques, including X-ray, CT, MRI, ultrasound, and nuclear medicine. My keen eye for detail and extensive knowledge empower me to extract vital information from medical images, enabling accurate diagnoses and informed treatment decisions.",
-                            "Contributions to Medical Science:",
-                            "In addition to clinical practice, I am an avid researcher and author. My work has been featured in numerous medical journals, and I have had the privilege of sharing insights at esteemed conferences. This commitment to advancing medical knowledge is a cornerstone of my approach to healthcare.",
-                            "Patient-Centered Care:",
-                            "Above all, I am deeply committed to providing patient-centric care. I believe in the power of empathy and open communication, ensuring that each individual feels heard, understood, and actively involved in their healthcare journey. My goal is to demystify complex medical information, empowering patients to make informed decisions about their health.",
-                            "Advocacy and Community Engagement:",
-                            "Beyond the confines of the hospital, I am a passionate advocate for health awareness and accessibility. Through community outreach initiatives, educational workshops, and proactive health screenings, I strive to make a positive impact on the well-being of individuals in our local communities.",
-                            "Beyond the Radiology Suite:",
-                            "In my downtime, I find solace in yoga, revel in the beauty of nature through hiking, and indulge my creative side through artistic pursuits. These activities offer balance and nourishment, allowing me to approach each day with renewed energy and a holistic perspective.",
-                            "Thank you for allowing me to share a glimpse of my journey and philosophy. I look forward to the privilege of being a part of your healthcare experience.",
-                            "Warm regards,",
-                            "Dr. Allison Mitchell, MD"],
-        },
-        {
-            picture: doctor2,
-            doctorAndTitle: "Dr. Benjamin Patel, MD",
-            specialization: "Neuroradiology",
-            additionalText: [
-                            "Greetings,",
-                            "I am Dr. Benjamin Patel, a distinguished radiologist with a singular focus on neuroradiology. For over a decade, I have dedicated myself to unraveling the mysteries of the brain through advanced imaging techniques, striving for precision, compassion, and excellence in patient care.",
-                            "Mastery in Neuroradiology:",
-                            "As a neuroradiologist, I have honed my skills in the interpretation of complex neuroimaging studies. My expertise spans a diverse array of imaging modalities, from MRI and CT to advanced techniques such as fMRI and DTI. This comprehensive approach allows me to provide precise diagnoses for a wide range of neurological conditions.",
-                            "Contributions to Neurological Science:",
-                            "Beyond my clinical practice, I am a fervent researcher, driven by a desire to push the boundaries of neurological science. My work has been featured in esteemed neurology journals, and I have had the privilege of presenting my findings at conferences worldwide.",
-                            "Patient-Centric Approach:",
-                            "I am deeply committed to patient-centered care. I believe in the power of empathy, active listening, and clear communication. My goal is to ensure that every individual I serve feels heard, understood, and empowered to make informed decisions about their neurological health.",
-                            "Advocacy and Education:",
-                            "In addition to my clinical responsibilities, I am an advocate for neurological health awareness. I actively engage with the community through educational workshops and outreach programs, seeking to demystify neurological conditions and promote early detection.",
-                            "Beyond the Lab:",
-                            "Outside of the radiology suite, I find rejuvenation in the practice of mindfulness and meditation. These practices not only enhance my focus and clarity in clinical settings but also serve as a foundation for a balanced, fulfilling life.",
-                            "Thank you for allowing me to share a glimpse of my journey and approach to healthcare. It would be an honor to be a part of your neuroimaging and neurological care.",
-                            "Warm regards,",
-                            "Dr. Benjamin Patel, MD"],
-        },
-        {
-            picture: doctor3,
-            doctorAndTitle: "Dr. Emily Rodriguez",
-            specialization: "Pediatric Radiology",
-            additionalText: [
-                            "Greetings,",
-                            "I am Dr. Emily Rodriguez, a devoted radiologist with a profound dedication to pediatric radiology. For over a decade, I have been privileged to play a crucial role in the healthcare journeys of young patients, combining expertise, empathy, and a passion for ensuring their optimal well-being.",
-                            "A Champion for Pediatric Radiology:",
-                            "As a pediatric radiologist, I specialize in imaging techniques tailored to the unique physiology and conditions of children. My expertise spans a range of modalities, from X-ray and ultrasound to MRI and CT, allowing for precise and accurate diagnoses for our young patients.",
-                            "Advancing Pediatric Healthcare:",
-                            "I believe in the power of research and education to shape the future of pediatric radiology. In addition to my clinical duties, I am an active contributor to research initiatives and have had the privilege of presenting my findings at esteemed pediatric radiology conferences.",
-                            "Nurturing Trusting Relationships:",
-                            "In my practice, I place a strong emphasis on patient and family-centered care. I recognize the importance of clear communication and understanding, ensuring that every young patient and their family feels supported, informed, and empowered throughout their healthcare journey.",
-                            "Advocacy for Pediatric Well-Being:",
-                            "Beyond the confines of the hospital, I am an advocate for pediatric health awareness. Through community outreach programs and educational initiatives, I work to foster a deeper understanding of pediatric conditions and the importance of early detection and intervention.",
-                            "Balancing Professional Excellence with Personal Fulfillment:",
-                            "Outside of the hospital, I find joy in spending quality time with my own family. This balance not only fuels my dedication to pediatric radiology but also enriches my own life, reinforcing the importance of holistic well-being.",
-                            "Thank you for allowing me to share a glimpse of my journey and philosophy. I am honored to be entrusted with the care of young lives and look forward to the privilege of contributing to their health and happiness.",
-                            "Warm regards,",
-                            "Dr. Emily Rodriguez, MD"],
-        },
-        {
-            picture: doctor4,
-            doctorAndTitle: "Dr. James Anderson, MD",
-            specialization: "Musculoskeletal Radiology",
-            additionalText: [
-                            "Greetings,",
-                            "I am Dr. James Anderson, a dedicated radiologist with a profound focus on musculoskeletal radiology. For over 15 years, I have honed my skills to provide precise diagnoses and comprehensive care for individuals with orthopedic and musculoskeletal conditions.",
-                            "Precision in Musculoskeletal Radiology:",
-                            "As a musculoskeletal radiologist, my expertise spans a spectrum of imaging techniques tailored to bones, joints, and soft tissues. From X-ray and MRI to CT and ultrasound, I employ these modalities to deliver accurate diagnoses and treatment plans for a wide range of orthopedic conditions.",
-                            "Advancing Orthopedic Care:",
-                            "I am committed to advancing orthopedic healthcare through research and innovation. My contributions to published research in esteemed orthopedic journals reflect my dedication to pushing the boundaries of musculoskeletal radiology.",
-                            "Patient-Centered Care:",
-                            "In my practice, I place great emphasis on patient-centered care. I understand the impact that musculoskeletal conditions can have on daily life, and I strive to provide compassionate, personalized care to every individual. Clear communication and empathy are at the heart of my approach.",
-                            "Advocacy for Orthopedic Wellness:",
-                            "Beyond clinical practice, I am an advocate for orthopedic health awareness. Through educational initiatives and community outreach programs, I aim to raise awareness about musculoskeletal conditions and the importance of early detection and intervention.",
-                            "Balancing Expertise with Personal Fulfillment:",
-                            "Outside of my professional endeavors, I find balance and fulfillment in various pursuits. Whether hiking through nature trails or enjoying quality time with loved ones, these moments serve as a reminder of the importance of holistic well-being.",
-                            "Thank you for allowing me to share a glimpse of my journey and philosophy. It is an honor to contribute to the orthopedic well-being of individuals, and I look forward to the privilege of providing expert care for those in need.",
-                            "Warm regards,",
-                            "Dr. James Anderson, MD"
-            ],
-        },
-        {
-            picture: doctor5,
-            doctorAndTitle: "Dr. Olivia Chang, MD",
-            specialization: "Thoracic Radiology",
-            additionalText: [
-                                "Greetings,","I am Dr. Olivia Chang, a devoted radiologist specializing in thoracic radiology. With over a decade of experience, I have dedicated myself to providing precise diagnoses and compassionate care for individuals with thoracic conditions.",
-                                "Mastery in Thoracic Radiology:",
-                                "As a thoracic radiologist, my expertise spans a spectrum of imaging techniques tailored to the chest, including X-ray, CT, MRI, and ultrasound. This comprehensive approach allows me to deliver accurate diagnoses and treatment plans for a wide range of thoracic conditions, from pulmonary disorders to cardiovascular issues.",
-                                "Contributions to Thoracic Healthcare:",
-                                "I am committed to advancing thoracic healthcare through research and innovation. My contributions to published research in esteemed thoracic journals reflect my dedication to pushing the boundaries of thoracic radiology.",
-                                "Patient-Centered Approach:",
-                                "In my practice, I place great emphasis on patient-centered care. I understand the impact that thoracic conditions can have on an individual's well-being and quality of life. I strive to provide compassionate, personalized care to every patient, ensuring they feel supported and informed throughout their healthcare journey.",
-                                "Advocacy for Thoracic Wellness:",
-                                "Beyond clinical practice, I am an advocate for thoracic health awareness. Through educational initiatives and community outreach programs, I aim to raise awareness about thoracic conditions and the importance of early detection and intervention.",
-                                "Balancing Expertise with Personal Fulfillment:",
-                                "Outside of my professional endeavors, I find balance and fulfillment in various pursuits. Whether exploring the outdoors, engaging in artistic endeavors, or spending quality time with loved ones, these moments serve as a reminder of the importance of holistic well-being.",
-                                "Thank you for allowing me to share a glimpse of my journey and philosophy. It is an honor to contribute to the thoracic well-being of individuals, and I look forward to the privilege of providing expert care for those in need.",
-                                "Warm regards,",
-                                "Dr. Olivia Chang, MD",
-                            ],
-        },
-        {
-            picture: doctor6,
-            doctorAndTitle: "Dr. Michael Nguyen, MD",
-            specialization: "Abdominal Radiology",
-            additionalText: [
-                            "Greetings,",
-                            "I am Dr. Michael Nguyen, a dedicated radiologist specializing in abdominal radiology. With a decade of experience, I have committed myself to providing accurate diagnoses and compassionate care for individuals with abdominal conditions.",
-                            "Mastery in Abdominal Radiology:",
-                            "As an abdominal radiologist, my expertise encompasses a range of imaging techniques tailored to the abdomen, including X-ray, CT, MRI, and ultrasound. This comprehensive approach empowers me to deliver precise diagnoses and treatment plans for a wide range of abdominal conditions, from gastrointestinal disorders to hepatic and renal issues.",
-                            "Contributions to Abdominal Healthcare:",
-                            "I am committed to advancing abdominal healthcare through research and innovation. My contributions to published research in esteemed abdominal journals reflect my dedication to pushing the boundaries of abdominal radiology.",
-                            "Patient-Centered Approach:",
-                            "In my practice, I place great emphasis on patient-centered care. I understand the impact that abdominal conditions can have on an individual's well-being and daily life. I strive to provide compassionate, personalized care to every patient, ensuring they feel supported and informed throughout their healthcare journey.",
-                            "Advocacy for Abdominal Wellness:",
-                            "Beyond clinical practice, I am an advocate for abdominal health awareness. Through educational initiatives and community outreach programs, I aim to raise awareness about abdominal conditions and the importance of early detection and intervention.",
-                            "Balancing Expertise with Personal Fulfillment:",
-                            "Outside of my professional endeavors, I find balance and fulfillment in various pursuits. Whether exploring culinary adventures, pursuing artistic endeavors, or spending quality time with loved ones, these moments serve as a reminder of the importance of holistic well-being.",
-                            "Thank you for allowing me to share a glimpse of my journey and philosophy. It is an honor to contribute to the abdominal well-being of individuals, and I look forward to the privilege of providing expert care for those in need.",
-                            "Warm regards,",
-                            "Dr. Michael Nguyen, MD"
-                            ],
-        },
-        ]
-        return doctors.map((item,index) =>{
-            return(
-                <SecondOpinionSection 
-                    picture={item.picture}
-                    doctorAndTitle={item.doctorAndTitle}
-                    specialization={item.specialization}
-                    additionalText={item.additionalText}
 
-                />
-            )
-            
+    const [doctors, setDoctors] = useState([]);
+    const { user } = useAuth();
+
+    useEffect(() => {
+        const API_URL = "https://dev-api.radiologyarchive.com";
+        const idToken = user.accessToken;
+        fetch(API_URL + "/api/user/radiologists" ,{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${idToken}`
+            }
         })
-    }
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                if (Array.isArray(data)){
+                    console.log("Response is an array:", data);
+                }
+                else{
+                    console.log("Response is not an array:", data);
+                }
+                
+                setDoctors(data.radiologists);
+            })
+            .catch((err) => console.error('Error fetching data:', err));
+    },[]);
 
+    return doctors && doctors.map((item, index) => {
+        return (
+            
+            <SecondOpinionSection 
+                key = {item.uid}
+                    picture = {item.profile_image_url}
+                    doctorAndTitle = {item.title + item.first_name + " " + item.last_name}
+                    specialization = {item.expertise}
+                    additionalText = {item.bio}
+            />
+            )
+        })
+            
+        
+
+    }
+    
+    
 export default SecondOpinionCenter;
--- src/components/second opinion components/SecondOpinionSection.jsx
+++ src/components/second opinion components/SecondOpinionSection.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 const SecondOpinionSection = ({picture,doctorAndTitle, specialization, additionalText}) => {
     const [showAdditionalText, setShowAdditionalText] = useState(false);
@@ -70,5 +69,79 @@ const SecondOpinionSection = ({picture,doctorAndTitle, specialization, additiona
       </div>
     );
   };
-  
-  export default SecondOpinionSection;
+
+  const wrapperStyle = {
+    border: "1px solid gray",
+    overflow: 'hidden',
+  };
+
+  const initialContainer = {
+    display: 'flex',
+    borderBottom: showAdditionalText ? '1px solid gray' : '',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+  const docImageStyle = {
+    margin: '10px',
+  };
+
+  const doctorTitleStyle = {
+    fontWeight: 'bold',
+    marginTop: '10px',
+  };
+
+  const buttonStyle = {
+    marginLeft: 'auto',
+    color: 'grey',
+    border: 'none',
+    padding: '10px',
+    fontSize: '16px',
+    width: '30px',
+    height: 'auto',
+    cursor: 'pointer',
+    display: 'center',
+    borderRadius: '5px',
+    marginRight: '2rem',
+  };
+
+  const additionalTextStyle = {
+    fontSize: '15px',
+  };
+
+  const additionalTextDiv = {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '10px',
+  };
+
+  // Split additionalText into an array of paragraphs
+  const additionalTextArray = additionalText.split('\n');
+
+  return (
+    <div style={wrapperStyle}>
+      <div style={initialContainer}>
+        <div>
+          <img src={picture} alt="Your Image" style={docImageStyle} />
+        </div>
+        <div>
+          <p style={doctorTitleStyle}>{doctorAndTitle}</p>
+          <p>Specialization: {specialization}</p>
+        </div>
+        <button style={buttonStyle} onClick={toggleAdditionalText}>
+          {showAdditionalText ? '^' : 'v'}
+        </button>
+      </div>
+      {showAdditionalText && (
+        <div style={additionalTextDiv}>
+          {additionalTextArray.map((paragraph, index) => (
+            <p key={index} style={additionalTextStyle}>
+              {paragraph}
+            </p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
