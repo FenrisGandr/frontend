e6b8ef508c0edf1286debbdbb9762f4280024ab1
thavisaj <145820872+thavisaj@users.noreply.github.com>
2023-11-01 01:54:27 -0700

    feat: radiologist updating their image notes (#25)

--- src/components/ImageView.jsx
+++ src/components/ImageView.jsx
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { API_URL } from "../constants.js";
 import { useAuth } from "../contexts/AuthContext";
 import Banner from "./Banner";
 import WebFooter from "./WebFooter";
 import DashboardSection from "./dashboard components/DashboardSection";
 
 export default function ImageView() {
-  const { state } = useLocation();
-  const image = state.image;
+  const { role, user } = useAuth();
+  const { pathname, state } = useLocation();
+  const navigate = useNavigate();
+  let opinionCount = 0;
+
+  const [image, setImage] = useState(state.image || {});
   const [showNote, setShowNote] = useState(false);
   const [showOpinions, setShowOpinions] = useState(false);
-  const [newNote, setNewNote] = useState("");
-  const { role } = useAuth();
-
+  const [newNote, setNewNote] = useState(
+    state.image.authors.find((author) => author.uid === user.uid)?.note || ""
+  );
 
   const roleColor = (role) => {
     if (role == "Radiologist") {
-        return "#E35D6A";
+      return "#E35D6A";
     }
     return "#0D6EFD"; // Physician color
-}
-  // Image URL
-  // const imageUrl = 'https://image.freepik.com/free-photo/spine-x-ray_1101-459.jpg';
+  };
 
   const textInputStyle = {
     height: "250px",
@@ -35,15 +38,47 @@ export default function ImageView() {
     display: "block",
     margin: "0 auto",
     textAlign: "left",
-};
+  };
   function handleChange(event) {
     setNewNote(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    // Add your code to handle form submission here
-    console.log("Note submitted:", newNote);
+    fetch(`${API_URL}/api/image/${image.uid}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        note: newNote,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + user.accessToken,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          const newAuthors = image.authors.map((author) => {
+            if (author.uid === user.uid) {
+              author.note = newNote;
+            }
+            return author;
+          });
+          setImage((prev) => ({
+            uid: prev.uid,
+            url: prev.url,
+            authors: newAuthors,
+          }));
+          // this is required to update the state of the image in the dashboard
+          // without this, the image will not show the updated note when refreshing
+          navigate(pathname, { state: { image } });
+        } else {
+          alert("Error updating note");
+        }
+      })
+      .catch((error) => {
+        console.log("Error updating note: ", error);
+      });
   }
 
   const containerStyle = {
@@ -52,7 +87,7 @@ export default function ImageView() {
     // width:"50%",
     // height:'auto',
     // gap: "15%",
-    marginBottom: "10rem"
+    marginBottom: "10rem",
   };
 
   const noteHeaderStyle = {
@@ -64,9 +99,8 @@ export default function ImageView() {
     height: "65px",
     display: "flex",
     alignItems: "center",
-    cursor: "pointer"
+    cursor: "pointer",
   };
-
 
   const opinionsHeaderStyle = {
     border: "1px solid #FF0000",
@@ -75,7 +109,7 @@ export default function ImageView() {
     height: "65px",
     display: "flex",
     alignItems: "center",
-    cursor: "pointer"
+    cursor: "pointer",
   };
 
   const opinionStyle = {
@@ -84,21 +118,21 @@ export default function ImageView() {
     // backgroundColor: "grey",
   };
 
-  const imageStyle={
+  const imageStyle = {
     width: "300px",
     height: "auto",
-    marginLeft: "40px"
-  }
+    marginLeft: "40px",
+  };
 
-  const opinionWrapperStlye ={
+  const opinionWrapperStlye = {
     marginLeft: "5rem",
-  }
+  };
 
-  const introStyle ={
-    color: '#0D6EFD',
-    margin: '3rem'
-  }
-  const buttonStyle= {
+  const introStyle = {
+    color: "#0D6EFD",
+    margin: "3rem",
+  };
+  const buttonStyle = {
     marginLeft: "auto",
     color: "grey",
     border: "none",
@@ -110,22 +144,22 @@ export default function ImageView() {
     display: "center",
     borderRadius: "5px",
     marginRight: "2rem",
-    backgroundColor: 'transparent'
-  }
+    backgroundColor: "transparent",
+  };
   const divider = {
-    height: '3px',
-    width: '50%',
-    backgroundColor: '#0D6EFD',
-    marginTop: '150px',
-    marginBottom: '50px',
-    marginLeft:'5rem'
-  }
+    height: "3px",
+    width: "50%",
+    backgroundColor: "#0D6EFD",
+    marginTop: "150px",
+    marginBottom: "50px",
+    marginLeft: "5rem",
+  };
   const spanStyle = {
     marginLeft: "100px",
-    fontWeight: 'bold',
-    fontSize: "17px"
-  }
-    const buttonStyle1= {
+    fontWeight: "bold",
+    fontSize: "17px",
+  };
+  const buttonStyle1 = {
     width: "250px",
     height: "40px",
     backgroundColor: roleColor(role),
@@ -150,12 +184,18 @@ export default function ImageView() {
 
       <div style={containerStyle}>
         <div>
-          <img className="p-5" style={imageStyle} src={image.url} alt="Medical Image" />
+          <img
+            className="p-5"
+            style={imageStyle}
+            src={image.url}
+            alt="Medical Image"
+          />
         </div>
         <div style={opinionWrapperStlye}>
-          <div style={noteHeaderStyle} onClick={()=>setShowNote(!showNote)}>
+          <div style={noteHeaderStyle} onClick={() => setShowNote(!showNote)}>
             <span style={spanStyle}>Your physician notes:</span>
-            <button style={buttonStyle}
+            <button
+              style={buttonStyle}
               onClick={() => {
                 setShowNote(!showNote);
               }}
@@ -172,7 +212,6 @@ export default function ImageView() {
                       <p>{author.note}</p>
                       <span>By: {author.full_name}</span>
                     </div>
-
                   );
                 }
               })
@@ -182,7 +221,10 @@ export default function ImageView() {
               </div>
             ))}
 
-          <div style={opinionsHeaderStyle} onClick={()=>setShowOpinions(!showOpinions)}>
+          <div
+            style={opinionsHeaderStyle}
+            onClick={() => setShowOpinions(!showOpinions)}
+          >
             <span style={spanStyle}>Radiologist Notes:</span>
             <button
               style={buttonStyle}
@@ -195,57 +237,63 @@ export default function ImageView() {
           </div>
           {showOpinions &&
             (image.authors && image.authors.length > 1 ? (
-              image.authors.map((author, index) => {
-                if (author.role === "RADIOLOGIST")
+              image.authors.map((author) => {
+                if (author.role === "RADIOLOGIST") {
+                  opinionCount++;
                   return (
                     <div key={author.uid} style={opinionStyle}>
-                      <p>
-                        Opinion {index}: {author.note}
+                      <p>Opinion {opinionCount}:</p>
+                      <p style={{ whiteSpace: "break-spaces" }}>
+                        {author.note}
                       </p>
                       <span>By: {author.full_name}</span>
                     </div>
                   );
+                }
               })
             ) : (
               <div style={opinionStyle}>
                 <p>No opinions</p>
-                </div>
+              </div>
             ))}
         </div>
       </div>
       {role === "Radiologist" && (
         <form onSubmit={handleSubmit}>
           <div style={yourNotesStyle}>
-          <label htmlFor="note">Your Notes:</label>
+            <label htmlFor="note">Your Notes:</label>
           </div>
           <br />
           <textarea
-          id="note"
-          type="text"
+            id="note"
+            type="text"
             name="note"
             value={newNote}
             onChange={handleChange}
-            style = {textInputStyle} //apply style to input
+            style={textInputStyle} //apply style to input
             rows={5}
           />
           <br />
-          <button type="submit" style={buttonStyle1}>Submit Analysis</button>
+          <button type="submit" style={buttonStyle1}>
+            Submit Analysis
+          </button>
         </form>
       )}
 
       {role === "Patient" && (
-
-      <div>
-        <div style={divider}></div>
-       <DashboardSection
-       role={role}
-       isLast={true}
-       headerDescription={"Want a Second Opinion?"}
-       paragraphDescription={"If you want a better understanding of your medical image, we provide a wide selection of radiologists for you! All you need to is select the radiologist that is right for you and they’ll be on their way to interpret your chosen image!"}
-       buttonDescription={"Get a Second Opinion"}
-       buttonLink={"/secondopinion"}
-       />
-       </div>
+        <div>
+          <div style={divider}></div>
+          <DashboardSection
+            role={role}
+            isLast={true}
+            headerDescription={"Want a Second Opinion?"}
+            paragraphDescription={
+              "If you want a better understanding of your medical image, we provide a wide selection of radiologists for you! All you need to is select the radiologist that is right for you and they’ll be on their way to interpret your chosen image!"
+            }
+            buttonDescription={"Get a Second Opinion"}
+            buttonLink={"/secondopinion"}
+          />
+        </div>
       )}
       <WebFooter />
     </>
