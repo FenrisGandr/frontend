eff3153f97ecdcfb0c0fc14cf207062a6929a198
thavisaj <005330373@coyote.csusb.edu>
2023-11-28 23:56:26 -0800

    feat: responsive nav bar & consolidate greeting card into banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,6 @@
 import React from "react";
+import "./Banner.css";
 
-export default function Banner( {text} ) {
-
-  const greetingStyle = {
-    paddingLeft: "50px",
-    backgroundColor: "#7749F8",
-    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-    color: "white",
-    height: "200px",
-    display: "flex",
-    alignItems: "center",
-    fontSize: "50px",
-  };
-
-  return <div style={greetingStyle}>{text}</div>;
+export default function Banner({ text }) {
+  return <div className="banner">{text}</div>;
 }
